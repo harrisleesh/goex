30a975beb98e9143ddb694b88bf5240f58f2a1d3
Harris Lee <harris@daangn.com>
2023-10-26 22:43:37 +0900

    Implement main function for RacingCar game inputs

--- RacingCar/main.go
+++ RacingCar/main.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world")
+	fmt.Println("자동차 대수는 몇 대인가요?")
+	var carCount, tryCount int
+	_, err := fmt.Scanf("%d", &carCount)
+	if err != nil {
+		return
+	}
+	fmt.Println("시도할 횟수는 몇 회인가요?")
+	_, err = fmt.Scanf("%d", &tryCount)
+	if err != nil {
+		return
+	}
 }
