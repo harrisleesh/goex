3911e8f9633ba35608b730961ffe86224a5bae76
Harris Lee <harris@daangn.com>
2023-10-30 23:34:28 +0900

    Create lotto domain struct

---
+++ Lotto/lotto/domain/lotto.go
@@ -0,0 +1,36 @@
+package domain
+
+import (
+	"math/rand"
+	"time"
+)
+
+type Lotto struct {
+	Numbers []int64
+}
+
+func NewLottos(count int64) []Lotto {
+	lottos := make([]Lotto, count)
+	for i := 0; i < int(count); i++ {
+		lottos[i] = newLotto()
+	}
+	return lottos
+}
+
+func newLotto() Lotto {
+	return Lotto{
+		Numbers: lottoNumbers()[0:6],
+	}
+}
+
+func lottoNumbers() []int64 {
+	arr := make([]int64, 45)
+	for i := 0; i < 45; i++ {
+		arr[i] = int64(i + 1)
+	}
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(arr), func(i, j int) {
+		arr[i], arr[j] = arr[j], arr[i]
+	})
+	return arr
+}
