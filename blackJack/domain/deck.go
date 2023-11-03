a71e5c7c869c82467d76ff896592b58374b05319
Harris Lee <harris@daangn.com>
2023-11-04 00:04:03 +0900

    IMPL DECK

--- blackJack/domain/deck.go
+++ blackJack/domain/deck.go
@@ -1,7 +1,16 @@
 package domain
 
-type deck []card
+type Deck []card
 
-func NewDeck() deck {
-	return deck{}
+func NewDeck() Deck {
+	cards := make([]card, 0)
+	for i := 1; i < 5; i++ {
+		for j := 1; j < 14; j++ {
+			cards = append(cards, card{
+				suit:   suit(i),
+				number: j,
+			})
+		}
+	}
+	return cards
 }
--- blackJack/domain/deck_test.go
+++ blackJack/domain/deck_test.go
@@ -6,11 +6,24 @@ import (
 )
 
 func TestNewDeck1(t *testing.T) {
+	cards := make([]card, 0)
+	for i := 1; i < 5; i++ {
+		for j := 1; j < 14; j++ {
+			cards = append(cards, card{
+				suit:   suit(i),
+				number: j,
+			})
+		}
+	}
 	tests := []struct {
 		name string
-		want deck
+		want Deck
 	}{
-		// TODO: Add test cases.
+
+		{
+			name: "deck 을 생성한다.",
+			want: cards,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
